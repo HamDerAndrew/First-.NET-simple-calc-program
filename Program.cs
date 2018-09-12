955ca33df913a1a2e41aa01ad639645a37528e6e
aSondergaard <andrewsondergaard@gmail.com>
2018-09-12 12:33:50 +0200

    cleaned up most of the calculator function, so there is less code

--- Program.cs
+++ Program.cs
@@ -10,27 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int result = calculator();
-            int result2 = calculator();
-            Console.WriteLine(result + result2);
+            //instruct user to input the numbers
+            Console.WriteLine("Enter Two numbers, which we'll add");
+            Console.WriteLine("Type the first number and hit Enter");
+            //Read first number
+            int number1 = int.Parse(Console.ReadLine());
+            //Read second number
+            Console.WriteLine("Enter the second number and hit Enter");
+            int number2 = int.Parse(Console.ReadLine());
+            //inform the user that the 2 numbers are going to be summed up
+            Console.WriteLine("Calculating... Please wait");
+            Console.WriteLine(calculator(number1, number2));
 
             Console.Read();
         }
-
+        
         static int calculator(int num1, int num2)
         {
-            int result = 0;
-
-            Console.WriteLine("Type two numbers. We'll add them");
-            int num1 = int.Parse(Console.ReadLine());
-            System.Threading.Thread.Sleep(1000);
-
-            Console.WriteLine("Now add the next number");
-            int num2 = int.Parse(Console.ReadLine());
-            System.Threading.Thread.Sleep(1500);
-
-            result = (num1 + num2);
-
+            int result = num1 + num2;
             return result;
 
         }
